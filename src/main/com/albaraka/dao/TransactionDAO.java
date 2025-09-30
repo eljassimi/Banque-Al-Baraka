2c462c17ecfc67d5d319fc8a6cb880a60779ca56
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-09-30 14:03:43 +0100

    ABB-7 jai implementer class TransactionDAO avec une function inserer

---
+++ src/main/com/albaraka/dao/TransactionDAO.java
@@ -0,0 +1,30 @@
+package main.com.albaraka.dao;
+
+import main.com.albaraka.config.DatabaseConfig;
+import main.com.albaraka.entity.Transaction;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+public class TransactionDAO {
+
+    public void insert(Transaction transaction) throws SQLException{
+         String sql = "INSERT INTO transaction (date, montant, type, lieu, idCompte) VALUES (?, ?, ?, ?, ?)";
+         try(Connection c = DatabaseConfig.getConnection(); PreparedStatement stmt = c.prepareStatement(sql)){
+
+             stmt.setTimestamp(1, Timestamp.valueOf(transaction.date()));
+             stmt.setDouble(2, transaction.montant());
+             stmt.setString(3, transaction.type().name());
+             stmt.setString(4, transaction.lieu());
+             stmt.setLong(5, transaction.idCompte());
+
+             int affectedRows = stmt.executeUpdate();
+             if (affectedRows == 0) {
+                 throw new SQLException("Echec de l'insertion du Transaction");
+             }
+         }
+    }
+
+}
