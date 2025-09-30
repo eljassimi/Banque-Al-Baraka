9ede9a6369db76d784ff31d11ae66cfe91075caf
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-09-30 20:46:18 +0100

    ABB-12 J'ai Implementer les fonction de filtre par Montant Type Date et Lieu

--- src/main/com/albaraka/service/TransactionService.java
+++ src/main/com/albaraka/service/TransactionService.java
@@ -1,10 +1,15 @@
 package main.com.albaraka.service;
 
 import main.com.albaraka.dao.TransactionDAO;
+import main.com.albaraka.entity.Compte;
 import main.com.albaraka.entity.Transaction;
+import main.com.albaraka.entity.TypeTransaction;
 
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TransactionService {
 
@@ -15,12 +20,27 @@ public class TransactionService {
     }
 
     public List<Transaction> listerTransactionsParCompte(Long idCompte) throws SQLException {
-        return transactionDAO.findByCompte(idCompte);
+        return transactionDAO.findByCompte(idCompte).stream().sorted(Comparator.comparing(Transaction::date)).toList();
     }
 
     public List<Transaction> listerTransactionsParClient(Long idClient) throws SQLException {
-        return transactionDAO.findByClient(idClient);
+        return transactionDAO.findByClient(idClient).stream().sorted(Comparator.comparing(Transaction::date)).toList();
     }
 
+    public List<Transaction> filtrerParMontant(double montantMin) throws SQLException {
+        return transactionDAO.findAll().stream().filter(e->e.montant()>=montantMin).collect(Collectors.toList());
+    }
+
+    public List<Transaction> filtrerParType(TypeTransaction type) throws SQLException {
+        return transactionDAO.findAll().stream().filter(e->e.type().equals(type)).collect(Collectors.toList());
+    }
+
+    public List<Transaction> filtrerParDate(LocalDateTime date) throws SQLException {
+        return transactionDAO.findAll().stream().filter(e->e.date().isAfter(date)).collect(Collectors.toList());
+    }
+
+    public List<Transaction> filtrerParLieu(String lieu) throws SQLException {
+        return transactionDAO.findAll().stream().filter(e->e.lieu().equals(lieu)).collect(Collectors.toList());
+    }
 
 }
