94913f4ec5cd8e31949679abd350ba8bf3bf6942
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-01 17:17:15 +0100

    ABB-20 ajouter le menu de client now je vais implementer les fonction

--- src/main/com/albaraka/ui/MenuClient.java
+++ src/main/com/albaraka/ui/MenuClient.java
@@ -1,6 +1,44 @@
 package main.com.albaraka.ui;
 
-public class MenuClient {
+import main.com.albaraka.service.ClientService;
 
-    public void afficher(){}
+import java.util.Scanner;
+
+public class MenuClient {
+    private final ClientService clientService;
+
+    public MenuClient() {
+        this.clientService = new ClientService();
+    }
+
+    public void afficher(){
+        Scanner sc = new Scanner(System.in);
+        boolean continuer = true;
+
+        while (continuer) {
+            System.out.println("------GESTION DES CLIENTS------");
+            System.out.println("1. Ajouter un client");
+            System.out.println("2. Modifier un client");
+            System.out.println("3. Supprimer un client");
+            System.out.println("4. Rechercher un client par ID");
+            System.out.println("5. Rechercher des clients par nom");
+            System.out.println("6. Lister tous les clients");
+            System.out.println("7. Statistiques d'un client");
+            System.out.println("0. Retour au menu principal");
+
+            int choix = sc.nextInt();
+
+                switch (choix) {
+                    case 1 -> ajouterClient();
+                    case 2 -> modifierClient();
+                    case 3 -> supprimerClient();
+                    case 4 -> rechercherParId();
+                    case 5 -> rechercherParNom();
+                    case 6 -> listerTousLesClients();
+                    case 7 -> afficherStatistiques();
+                    case 0 -> continuer = false;
+                    default -> System.out.println("Choix invalide");
+                }
+        }
+    }
 }
